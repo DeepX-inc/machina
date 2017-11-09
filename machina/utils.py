649349be65a38b73d870a830f40e0a14e58189e5
rarilurelo <rarilurelo@gmail.com>
2017-11-09 21:35:50 +0900

    add cpu mode for sampler

--- machina/utils.py
+++ machina/utils.py
@@ -1,3 +1,4 @@
+import contextlib
 import torch.autograd as autograd
 
 # default gpu_id is -1.
@@ -7,6 +8,16 @@ gpu_id = -1
 def set_gpu(device_id):
     global gpu_id
     gpu_id = device_id
+
+
+@contextlib.contextmanager
+def cpu_mode():
+    global gpu_id
+    _gpu_id = gpu_id
+    gpu_id = -1
+    yield
+    gpu_id = _gpu_id
+
 
 class Variable(autograd.Variable):
     def __init__(self, data, *args, **kwargs):
