1505eecfb51747dd28b046fcbb49ef2122a6b848
Unknown <yoshida.taketo1995@gmail.com>
2018-04-24 15:15:35 +0900

    import prioritized replay data

--- machina/data/__init__.py
+++ machina/data/__init__.py
@@ -1,3 +1,4 @@
 from machina.data.base import BaseData
 from machina.data.gae_data import GAEData
 from machina.data.replay_data import ReplayData
+from machina.data.prioritized_replay_data import PrioritizedReplayData
