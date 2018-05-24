f3bee706441bf65a8aad0fc72784d9e0dcbc6776
Unknown <yoshida.taketo1995@gmail.com>
2018-05-24 16:04:49 +0900

    change how sample

--- machina/data/expert_data.py
+++ machina/data/expert_data.py
@@ -15,22 +15,19 @@ class ExpertData(BaseData):
         self.num_of_traj = self.expert_data['obs'].shape[0]
         self.num_of_step = self.expert_data['obs'].shape[1]
 
-    def iterate(self, batch_size, epoch=10):
-        full_indices = np.arange(self.num_of_traj * self.num_of_step, dtype=np.int) + 1
-        full_indices = full_indices[np.where(full_indices%self.num_of_step!=0)]
-        full_indices = full_indices -1
+    def iterate(self, batch_size):
+        full_indices = np.array([[traj_indice, step_indice] for traj_indice in range(self.num_of_traj) for step_indice in range(self.num_of_step-1)])
         full_indices_shuffled = np.random.permutation(full_indices)
-        indices_minibatches = np.array_split(full_indices_shuffled, len(full_indices) // batch_size)
-        for iteration, indices_minibatch in enumerate(indices_minibatches):
-            indices_minibatch_traj = np2torch(indices_minibatch // self.num_of_step).long()
-            indices_minibatch_step = np2torch(indices_minibatch % self.num_of_step).long()
+        indices_split = np.array_split(full_indices_shuffled, len(full_indices) // batch_size)
+        for iteration, indices_minibatch in enumerate(indices_split):
+            indices_minibatch_traj = np2torch(indices_minibatch.T[0]).long()
+            indices_minibatch_step = np2torch(indices_minibatch.T[1]).long()
             yield iteration,\
                     dict(
                         obs=self.obs[indices_minibatch_traj,indices_minibatch_step],
                         next_obs=self.obs[indices_minibatch_traj, indices_minibatch_step + 1],
                         acs=self.acs[indices_minibatch_traj, indices_minibatch_step]
                 )
-
 
 
 if __name__=='__main__':
@@ -40,7 +37,7 @@ if __name__=='__main__':
     parser.add_argument('--file_name', type=str, default='halfcheetah_rllab_3232_noBN_0202pol_max_HalfCheetah-v1_30trajs.npz')
     args = parser.parse_args()
     expertdata = ExpertData(os.path.join(os.getcwd(),args.dir, args.file_name))
-    for iteration, batch in expertdata.iterate(batch_size=100,epoch=2):
+    for iteration, batch in expertdata.iterate(batch_size=100):
 #    for batch in expertdata.iterate(batch_size=3,num_of_step=1, epoch=2):
         print('iteration={}, batchsize={}'.format(iteration, len(batch['obs'])))
     print(expertdata.obs)
