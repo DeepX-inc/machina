f5ac369781c4e9930703de6661b1fea808621e8d
Kaoru Nasuno Local <kaorun423@gmail.com>
2018-06-26 15:47:29 +0900

    adding a simple description of algos

--- machina/algos/svg.py
+++ machina/algos/svg.py
@@ -12,6 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+#
+# This is an implementation of Stochastic Value Gradient.
+# See https://arxiv.org/abs/1510.09142
+#
 
 import torch
 import torch.nn as nn
--- machina/algos/vpg.py
+++ machina/algos/vpg.py
@@ -12,6 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+#
+# This is an implementation of Vanilla Policy Gradient.
+#
+
 
 import torch
 import torch.nn as nn
