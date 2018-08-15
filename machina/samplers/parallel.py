908e06d17da7f59bb1dfbe3b4df67544544a0ce7
rarilurelo <rarilurelo@gmail.com>
2018-08-15 13:07:47 +0900

    fix

--- machina/samplers/parallel.py
+++ machina/samplers/parallel.py
@@ -53,7 +53,7 @@ def sample_process(pol, env, max_samples, max_episodes, n_samples_global, n_epis
 class ParallelSampler(BaseSampler):
     def __init__(self, env, pol, max_samples, max_episodes, num_parallel=8, prepro=None):
         BaseSampler.__init__(self, env)
-        self.pol = copy.deepcopy(pol)
+        self.pol = copy.deepcopy(pol.cpu())
         self.pol.share_memory()
         self.max_samples = max_samples
         self.max_episodes = max_episodes
