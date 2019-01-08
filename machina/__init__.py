dc0f349b0e31f1a81965b8457df67a9f32f979d7
rarilurelo <rarilurelo@gmail.com>
2019-01-08 15:05:11 +0900

    fix

--- machina/__init__.py
+++ machina/__init__.py
@@ -13,5 +13,4 @@
 # limitations under the License.
 # ==============================================================================
 
-from machina import misc  # NOQA
 from machina.traj import epi_functional
