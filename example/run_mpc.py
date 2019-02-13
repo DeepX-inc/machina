60f570345d41e527f5a8d90023b81095b5285a45
jinbeizame007 <jinbeizame007@gmail.com>
2019-02-13 20:18:08 +0900

    add run_mpc.py

---
+++ example/run_mpc.py
@@ -0,0 +1,191 @@
+"""
+An example of Model Predictive Control.
+"""
+
+import argparse
+import json
+import os
+from pprint import pprint
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import gym
+
+import machina as mc
+from machina.pols import GaussianPol, CategoricalPol, MultiCategoricalPol
+from machina.algos import trpo
+from machina.vfuncs import DeterministicSVfunc
+from machina.envs import GymEnv, C2DEnv
+from machina.traj import Traj
+from machina.traj import epi_functional as ef
+from machina.traj import traj_functional as tf
+from machina.samplers import EpiSampler
+from machina import logger
+from machina.utils import measure
+
+from simple_net import PolNet, VNet, PolNetLSTM, VNetLSTM
+
+
+class RandomPolicy(nn.Module):
+    def __init__(self, action_space):
+        super(RandomPolicy, self).__init__()
+        self.low_val = action_space.low
+        self.high_val = action_space.high
+        self.shape = action_space.shape
+    
+    def forward(self, ob):
+        ac_real = np.random.uniform(self.low_val, self.high_val, self.shape, dtype=np.float32)
+        ac = torch.tensor(ac_real)
+        mean = torch.zeros_like(ac)
+        return ac_real, ac, dict(mean=mean)
+
+def add_noise_to_init_ob(data, std):
+    epis = data.current_epis
+    with torch.no_grad():
+        for epi in epis:
+            epis['obs'][0] += torch.normal(mean=0, std=torch.full_like(epi['obs'][0], std))
+    return data
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--log', type=str, default='garbage')
+parser.add_argument('--env_name', type=str, default='Pendulum-v0')
+parser.add_argument('--c2d', action='store_true', default=False)
+parser.add_argument('--record', action='store_true', default=False)
+parser.add_argument('--episode', type=int, default=1000000)
+parser.add_argument('--seed', type=int, default=256)
+parser.add_argument('--max_episodes', type=int, default=1000000)
+parser.add_argument('--num_rollouts_train', type=int, default=10)
+parser.add_argument('--num_rollouts_val', type=int, default=20)
+parser.add_argument('--num_parallel', type=int, default=4)
+
+parser.add_argument('--max_steps_per_iter', type=int, default=10000)
+parser.add_argument('--epoch_per_iter', type=int, default=5)
+parser.add_argument('--batch_size', type=int, default=64)
+parser.add_argument('--vf_lr', type=float, default=3e-4)
+parser.add_argument('--rnn', action='store_true', default=False)
+
+parser.add_argument('--gamma', type=float, default=0.995)
+parser.add_argument('--lam', type=float, default=1)
+args = parser.parse_args()
+
+if not os.path.exists(args.log):
+    os.mkdir(args.log)
+
+with open(os.path.join(args.log, 'args.json'), 'w') as f:
+    json.dump(vars(args), f)
+pprint(vars(args))
+
+if not os.path.exists(os.path.join(args.log, 'models')):
+    os.mkdir(os.path.join(args.log, 'models'))
+
+np.random.seed(args.seed)
+torch.manual_seed(args.seed)
+
+score_file = os.path.join(args.log, 'progress.csv')
+logger.add_tabular_output(score_file)
+
+env = GymEnv(args.env_name, log_dir=os.path.join(
+    args.log, 'movie'), record_video=args.record)
+env.env.seed(args.seed)
+if args.c2d:
+    env = C2DEnv(env)
+
+ob_space = env.observation_space
+ac_space = env.action_space
+
+if args.rnn:
+    pol_net = PolNetLSTM(ob_space, ac_space, h_size=256, cell_size=256)
+else:
+    pol_net = PolNet(ob_space, ac_space)
+if isinstance(ac_space, gym.spaces.Box):
+    pol = GaussianPol(ob_space, ac_space, pol_net, args.rnn)
+elif isinstance(ac_space, gym.spaces.Discrete):
+    pol = CategoricalPol(ob_space, ac_space, pol_net, args.rnn)
+elif isinstance(ac_space, gym.spaces.MultiDiscrete):
+    pol = MultiCategoricalPol(ob_space, ac_space, pol_net, args.rnn)
+else:
+    raise ValueError('Only Box, Discrete, and MultiDiscrete are supported')
+
+random_pol = RandomPolicy(ac_space)
+
+if args.rnn:
+    vf_net = VNetLSTM(ob_space, h_size=256, cell_size=256)
+else:
+    vf_net = VNet(ob_space)
+vf = DeterministicSVfunc(ob_space, vf_net, args.rnn)
+
+sampler = EpiSampler(env, pol, num_parallel=args.num_parallel, seed=args.seed)
+optim_vf = torch.optim.Adam(vf_net.parameters(), args.vf_lr)
+
+############################
+### Train Dynamics Model ###
+############################
+
+### Prepare the dataset D_RAND ###
+
+# Performing rollouts to collect training data
+epis = sampler.sample(random_pol, max_episodes=args.num_rollouts_train)
+epis = add_noise_to_init_ob(epis)
+rand_traj_train = Traj()
+rand_traj_train.add_epis(epis)
+rand_traj_train = ef.add_next_obs(rand_traj_train)
+rand_traj_train.register_epis()
+
+epis = sampler.sample(random_pol, max_episodes=args.num_rollouts_val)
+epis = add_noise_to_init_ob(epis)
+rand_traj_val = Traj()
+rand_traj_val.add_epis(epis)
+rand_traj_val = ef.add_next_obs(rand_traj_val)
+rand_traj_val.register_epis()
+
+### 
+
+total_epi = 0
+total_step = 0
+max_rew = -1e6
+while args.max_episodes > total_epi:
+    with measure('sample'):
+        epis = sampler.sample(pol, max_steps=args.max_steps_per_iter)
+    with measure('train'):
+        traj = Traj()
+        traj.add_epis(epis)
+
+        traj = ef.compute_vs(traj, vf)
+        traj = ef.compute_rets(traj, args.gamma)
+        traj = ef.compute_advs(traj, args.gamma, args.lam)
+        traj = ef.centerize_advs(traj)
+        traj = ef.compute_h_masks(traj)
+        traj.register_epis()
+
+        result_dict = trpo.train(
+            traj, pol, vf, optim_vf, args.epoch_per_iter, args.batch_size)
+
+    total_epi += traj.num_epi
+    step = traj.num_step
+    total_step += step
+    rewards = [np.sum(epi['rews']) for epi in epis]
+    mean_rew = np.mean(rewards)
+    logger.record_results(args.log, result_dict, score_file,
+                          total_epi, step, total_step,
+                          rewards,
+                          plot_title=args.env_name)
+
+    if mean_rew > max_rew:
+        torch.save(pol.state_dict(), os.path.join(
+            args.log, 'models', 'pol_max.pkl'))
+        torch.save(vf.state_dict(), os.path.join(
+            args.log, 'models', 'vf_max.pkl'))
+        torch.save(optim_vf.state_dict(), os.path.join(
+            args.log, 'models', 'optim_vf_max.pkl'))
+        max_rew = mean_rew
+
+    torch.save(pol.state_dict(), os.path.join(
+        args.log, 'models', 'pol_last.pkl'))
+    torch.save(vf.state_dict(), os.path.join(
+        args.log, 'models', 'vf_last.pkl'))
+    torch.save(optim_vf.state_dict(), os.path.join(
+        args.log, 'models', 'optim_vf_last.pkl'))
+    del traj
+del sampler
--- machina/traj/traj_functional.py
+++ machina/traj/traj_functional.py
@@ -11,3 +11,8 @@ def update_pris(traj, td_loss, indices, alpha=0.6, epsilon=1e-6):
     pris = (torch.abs(td_loss) + epsilon) ** alpha
     traj.data_map['pris'][indices] = pris.detach()
     return traj
+
+def add_noise_to_obs(traj, std):
+    traj.data_map['obs'] = torch.normal(mean=traj.data_map['obs'], std=torch.full_like(traj.data_map['obs'], std))
+    traj.data_map['next_obs'] = torch.normal(mean=traj.data_map['next_obs'], std=torch.full_like(traj.data_map['next_obs'], std))
+    return traj
