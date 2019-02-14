cc3ff15fe1c39c859e9a86d6fd708e0bfc918793
jinbeizame007 <jinbeizame007@gmail.com>
2019-02-14 19:38:32 +0900

    add statistic to args of normalize function

--- machina/traj/traj_functional.py
+++ machina/traj/traj_functional.py
@@ -13,21 +13,31 @@ def update_pris(traj, td_loss, indices, alpha=0.6, epsilon=1e-6):
     return traj
 
 
-def normalize_obs_and_acs(traj):
+def normalize_obs_and_acs(traj, mean_obs=None, std_obs=None, mean_acs=None, std_acs=None, mean_next_obs=None, std_next_obs=None, return_statistic=True):
     with torch.no_grad():
-        mean_obs = torch.mean(traj.data_map['obs'], dim=0, keepdim=True)
-        std_obs = torch.std(traj.data_map['obs'], dim=0, keepdim=True)
-        traj.data_map['obs'] = (traj.data_map['obs'] - mean_obs) / std_obs
+        if mean_obs is None:
+            mean_obs = torch.mean(traj.data_map['obs'], dim=0, keepdim=True)
+        if std_obs is None:
+            std_obs = torch.std(traj.data_map['obs'], dim=0, keepdim=True)
 
-        mean_next_obs = torch.mean(
-            traj.data_map['next_obs'], dim=0, keepdim=True)
+        if mean_acs is None:
+            mean_acs = torch.mean(traj.data_map['acs'], dim=0, keepdim=True)
+        if std_acs is None:
+            std_acs = torch.std(traj.data_map['acs'], dim=0, keepdim=True)
+
+        if std_next_obs is None:
+            mean_next_obs = torch.mean(
+                traj.data_map['next_obs'], dim=0, keepdim=True)
+        if std_next_obs is None
         std_next_obs = torch.std(
             traj.data_map['next_obs'], dim=0, keepdim=True)
+
+        traj.data_map['obs'] = (traj.data_map['obs'] - mean_obs) / std_obs
+        traj.data_map['acs'] = (traj.data_map['acs'] - mean_acs) / std_acs
         traj.data_map['next_obs'] = (
             traj.data_map['next_obs'] - mean_next_obs) / std_next_obs
 
-        mean_acs = torch.mean(traj.data_map['acs'], dim=0, keepdim=True)
-        std_acs = torch.std(traj.data_map['acs'], dim=0, keepdim=True)
-        traj.data_map['acs'] = (traj.data_map['acs'] - mean_acs) / std_acs
-
-    return traj, mean_obs, std_obs, mean_next_obs, std_next_obs, mean_acs, std_acs
+    if return_statistics:
+        return traj, mean_obs, std_obs, mean_acs, std_acs, mean_next_obs, std_next_obs
+    else:
+        return traj
