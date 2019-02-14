c56ecf55bf4203563cadcccd001bea70fa71728e
jinbeizame007 <jinbeizame007@gmail.com>
2019-02-14 14:09:51 +0900

    add mpc_pol.py

---
+++ machina/pols/mpc_pol.py
@@ -0,0 +1,80 @@
+import numpy as np
+import torch
+
+from machina.pds import DeterministicPd
+from machina.pols import BasePol
+from machina.utils import get_device
+
+
+class MPCPol(BasePol):
+    """
+    Policy with model predictive control.
+
+    Parameters
+    ----------
+    ob_space : gym.Space
+        observation's space
+    ac_space : gym.Space
+        action's space.
+        This should be gym.spaces.Box
+    net : torch.nn.Module
+        dymamics model
+    rew_func : function
+        rt = rew_func(st+1, at). rt, st+1 and at are torch.Tensor.
+    env : gym.Env
+    n_samples : int
+    horizon : int
+    rnn : bool
+    normalize_ac : bool
+        If True, the output of network is spreaded for ac_space.
+        In this situation the output of network is expected to be in -1~1.
+    data_parallel : bool
+        If True, network computation is executed in parallel.
+    parallel_dim : int
+        Splitted dimension in data parallel.
+    """
+
+    def __init__(self, ob_space, ac_space, net, rew_func, env, n_samples=1000, horizon=20, rnn=False, normalize_ac=True, data_parallel=False, parallel_dim=0):
+        if rnn:
+            raise ValueError(
+                'rnn with MPCPol is not supported now')
+        BasePol.__init__(self, ob_space, ac_space, net, rnn=rnn, normalize_ac=normalize_ac,
+                         data_parallel=data_parallel, parallel_dim=parallel_dim)
+        self.env = env
+        self.n_samples = n_samples
+        self.horizon = horizon
+        self.to(get_device())
+
+    def reset(self):
+        super(MPCPol, self).reset()
+
+    def forward(self, obs):
+        # randomly sample N candidate action sequences
+        sample_acs = np.random.uniform(
+            self.ac_space.low, self.ac_space.high, (self.horizon, self.n_samples, self.ac_space.shape[0]))
+        sample_acs = torch.tensor(samples_acs, device=get_device())
+
+        # forward simulate the action sequences to get predicted trajectories
+        obs = torch.zeros((self.horizon+1, self.n_samples,
+                           self.ob_space.shape[0]), dtype=torch.float, device=get_device())
+        rews_sum = torch.zeros(
+            (self.n_samples), dtype=torch.float, device=get_device())
+        obs[0] = torch.tensor(self.env.reset(), device=get_device())
+        obs[0] = self._check_obs_shape(obs[0])
+        with torch.no_grad():
+            for i in range(self.horizon):
+                obs[i+1] = obs[i] + self.net(obs[i], sample_acs[i])
+                rews_sum += rew_func(obs[i+1], )
+
+        best_sample_index = rews_sum.max(0)[1]
+        ac = all_sample_acs[0][best_sample_index]
+        ac_real = ac.cpu().numpy()
+
+        return ac_real, ac, dict(mean=ac)
+
+    def deterministic_ac_real(self, obs):
+        """
+        action for deployment
+        """
+        mean_read, mean, dic = self.forward(obs)
+        return mean_real, mean, dict(mean=mean)
