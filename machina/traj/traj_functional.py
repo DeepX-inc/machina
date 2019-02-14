8ab7c5f403643fc4cf7acd4c113df60a4acada7a
jinbeizame007 <jinbeizame007@gmail.com>
2019-02-14 10:57:38 +0900

    fix

--- machina/traj/traj_functional.py
+++ machina/traj/traj_functional.py
@@ -12,15 +12,19 @@ def update_pris(traj, td_loss, indices, alpha=0.6, epsilon=1e-6):
     traj.data_map['pris'][indices] = pris.detach()
     return traj
 
+
 def normalize_obs_and_acs(traj):
     with torch.no_grad():
         mean_obs = torch.mean(traj.data_map['obs'], dim=0, keepdim=True)
         std_obs = torch.std(traj.data_map['obs'], dim=0, keepdim=True)
         traj.data_map['obs'] = (traj.data_map['obs'] - mean_obs) / std_obs
 
-        mean_next_obs = torch.mean(traj.data_map['next_obs'], dim=0, keepdim=True)
-        std_next_obs = torch.std(traj.data_map['next_obs'], dim=0, keepdim=True)
-        traj.data_map['next_obs'] = (traj.data_map['next_obs'] - mean_next_obs) / std_next_obs
+        mean_next_obs = torch.mean(
+            traj.data_map['next_obs'], dim=0, keepdim=True)
+        std_next_obs = torch.std(
+            traj.data_map['next_obs'], dim=0, keepdim=True)
+        traj.data_map['next_obs'] = (
+            traj.data_map['next_obs'] - mean_next_obs) / std_next_obs
 
         mean_acs = torch.mean(traj.data_map['acs'], dim=0, keepdim=True)
         std_acs = torch.std(traj.data_map['acs'], dim=0, keepdim=True)
