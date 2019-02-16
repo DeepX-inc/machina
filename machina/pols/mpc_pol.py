3500365e64ffbb64ae22dc8a15efd534f74ed5cc
jinbeizame007 <jinbeizame007@gmail.com>
2019-02-17 01:35:27 +0900

    apply mpc_pol to cpu

--- machina/pols/mpc_pol.py
+++ machina/pols/mpc_pol.py
@@ -48,13 +48,13 @@ class MPCPol(BasePol):
         self.rew_func = rew_func
         self.n_samples = n_samples
         self.horizon = horizon
-        self.mean_obs = mean_obs.repeat(n_samples, 1)
-        self.std_obs = std_obs.repeat(n_samples, 1)
-        self.mean_acs = mean_acs.repeat(n_samples, 1)
-        self.std_acs = std_acs
-        self.mean_next_obs = mean_next_obs
-        self.std_next_obs = std_next_obs
         self.to(get_device())
+        self.mean_obs = mean_obs.repeat(n_samples, 1).to('cpu')
+        self.std_obs = std_obs.repeat(n_samples, 1).to('cpu')
+        self.mean_acs = mean_acs.repeat(n_samples, 1).to('cpu')
+        self.std_acs = std_acs.to('cpu')
+        self.mean_next_obs = mean_next_obs.to('cpu')
+        self.std_next_obs = std_next_obs.to('cpu')
 
     def reset(self):
         super(MPCPol, self).reset()
@@ -64,15 +64,14 @@ class MPCPol(BasePol):
         sample_acs = np.random.uniform(
             self.ac_space.low[0], self.ac_space.high[0], (self.horizon, self.n_samples, self.ac_space.shape[0]))
         sample_acs = torch.tensor(
-            sample_acs, dtype=torch.float, device=get_device())
+            sample_acs, dtype=torch.float)
 
         # forward simulate the action sequences to get predicted trajectories
         obs = torch.zeros((self.horizon+1, self.n_samples,
-                           self.ob_space.shape[0]), dtype=torch.float, device=get_device())
+                           self.ob_space.shape[0]), dtype=torch.float)
         rews_sum = torch.zeros(
-            (self.n_samples), dtype=torch.float, device=get_device())
+            (self.n_samples), dtype=torch.float)
 
-        #obs[0] = torch.tensor(self.env.reset(), device=get_device())
         obs[0] = ob.repeat(self.n_samples, 1)
         obs[0] = self._check_obs_shape(obs[0])
         with torch.no_grad():
@@ -80,10 +79,8 @@ class MPCPol(BasePol):
                 ob = (obs[i] - self.mean_obs) / self.std_obs
                 ac = (sample_acs[i] - self.mean_acs) / self.std_acs
                 # inf to mean
-                ob[ob == float('inf')
-                   ] = 0.  # self.mean_obs[ob == float('inf')]
-                ac[ac == float('inf')
-                   ] = 0.  # self.mean_acs[ac == float('inf')]
+                ob[ob == float('inf')] = 0.
+                ac[ac == float('inf')] = 0.
                 next_ob = ob + self.net(ob, ac)
                 obs[i+1] = next_ob * self.std_next_obs + self.mean_next_obs
                 rews_sum += self.rew_func(obs[i+1], sample_acs[i])
