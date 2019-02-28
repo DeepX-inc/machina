be9d714283106984f0a658a212aeb772377b1012
pierrewuethrich <wuethrich.pierre@gmail.com>
2019-02-28 13:43:44 +0900

    Add distillation policy script

--- machina/distillation_loss.py
+++ machina/distillation_loss.py
@@ -2,4 +2,9 @@
 Losses for Policy distillation
 """
 
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from machina.utils import detach_tensor_dict, get_device
 
