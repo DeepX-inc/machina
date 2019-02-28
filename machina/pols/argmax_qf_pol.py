0a0a22b652852e942ecdb5f0a8417c52cc440800
takerfume <yoshida.taketo1995@gmail.com>
2019-02-28 22:16:11 +0900

    Remove discrepancy of pol and env

--- machina/pols/argmax_qf_pol.py
+++ machina/pols/argmax_qf_pol.py
@@ -33,20 +33,20 @@ class ArgmaxQfPol(BasePol):
         Probability of random action
     """
 
-    def __init__(self, ob_space, ac_space, qfunc, rnn=False, normalize_ac=True, data_parallel=False, parallel_dim=0, eps=0.2, env=None):
+    def __init__(self, ob_space, ac_space, qfunc, rnn=False, normalize_ac=True, data_parallel=False, parallel_dim=0, eps=0.2):
         BasePol.__init__(self, ob_space, ac_space, None, rnn,
                          normalize_ac, data_parallel, parallel_dim)
         self.qfunc = qfunc
         self.eps = eps
-        self.env = env
         self.to(get_device())
 
     def forward(self, obs):
         prob = random.uniform(0., 1.)
         if prob <= self.eps:
             ac_real = ac = torch.tensor(
-                self.env.action_space.sample(), dtype=torch.float, device=get_device())
+                self.ac_space.sample(), dtype=torch.float, device=obs.device)
+            q, _ = self.qfunc(obs, ac)
         else:
             q, ac = self.qfunc.max(obs)
             ac_real = self.convert_ac_for_real(ac.detach().cpu().numpy())
-        return ac_real, ac, dict()
+        return ac_real, ac, dict(q=q)
