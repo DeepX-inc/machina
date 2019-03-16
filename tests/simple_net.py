223483397dca0815baadcf4e1bed79a9121a387e
jinbeizame007 <jinbeizame007@gmail.com>
2019-03-16 20:21:26 +0900

    add QNetLSTM to tests

--- tests/simple_net.py
+++ tests/simple_net.py
@@ -221,6 +221,45 @@ class VNetLSTM(nn.Module):
         return outs, hs
 
 
+class QNetLSTM(nn.Module):
+    def __init__(self, ob_space, ac_space, h_size=1024, cell_size=512):
+        super(QNetLSTM, self).__init__()
+        self.h_size = h_size
+        self.cell_size = cell_size
+        self.rnn = True
+
+        self.input_layer = nn.Linear(
+            ob_space.shape[0] + ac_space.shape[0], self.h_size)
+        self.cell = nn.LSTMCell(self.h_size, hidden_size=self.cell_size)
+        self.output_layer = nn.Linear(self.cell_size, 1)
+
+        self.output_layer.apply(mini_weight_init)
+
+    def init_hs(self, batch_size=1):
+        new_hs = (next(self.parameters()).new(batch_size, self.cell_size).zero_(), next(
+            self.parameters()).new(batch_size, self.cell_size).zero_())
+        return new_hs
+
+    def forward(self, ob, ac, hs, h_masks):
+        time_seq, batch_size, *_ = ob.shape
+
+        hs = (hs[0].reshape(batch_size, self.cell_size),
+              hs[1].reshape(batch_size, self.cell_size))
+
+        xs = torch.cat([ob, ac], dim=-1)
+        xs = torch.relu(self.input_layer(xs))
+
+        hiddens = []
+        for x, mask in zip(xs, h_masks):
+            hs = (hs[0] * (1 - mask), hs[1] * (1 - mask))
+            hs = self.cell(x, hs)
+            hiddens.append(hs[0])
+        hiddens = torch.cat([h.unsqueeze(0) for h in hiddens], dim=0)
+        outs = self.output_layer(hiddens)
+
+        return outs, hs
+
+
 class ModelNetLSTM(nn.Module):
     def __init__(self, ob_space, ac_space, h_size=1024, cell_size=512):
         super(ModelNetLSTM, self).__init__()
