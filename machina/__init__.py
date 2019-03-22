477d2f7dc965c05255156a131eef845412368b74
iory <ab.ioryz@gmail.com>
2019-03-22 17:00:59 +0900

    [__init__.py] Add import machina library

--- machina/__init__.py
+++ machina/__init__.py
@@ -4,4 +4,14 @@ import pkg_resources
 __version__ = pkg_resources.get_distribution('machina-rl').version
 
 
-from machina.traj import epi_functional
+from machina import algos  # NOQA
+from machina import envs  # NOQA
+from machina import models  # NOQA
+from machina import noise  # NOQA
+from machina import optims  # NOQA
+from machina import pds  # NOQA
+from machina import pols  # NOQA
+from machina import prepro  # NOQA
+from machina import samplers  # NOQA
+from machina import traj  # NOQA
+from machina import vfuncs  # NOQA
