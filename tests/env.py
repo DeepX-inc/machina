fa9a5bf982faa590e563bc00e7319bdf51f4dd15
takerfume <yoshida.taketo1995@gmail.com>
2019-03-28 17:03:21 +0900

    [tests/env] Add dict space

--- tests/env.py
+++ tests/env.py
@@ -7,21 +7,29 @@ from gym.utils import seeding
 import numpy as np
 from os import path
 
+
 class PendulumDictEnv(gym.Env):
     metadata = {
-        'render.modes' : ['human', 'rgb_array'],
-        'video.frames_per_second' : 30
+        'render.modes': ['human', 'rgb_array'],
+        'video.frames_per_second': 30
     }
 
     def __init__(self):
-        self.max_speed=8
-        self.max_torque=2.
-        self.dt=.05
+        self.max_speed = 8
+        self.max_torque = 2.
+        self.dt = .05
         self.viewer = None
 
-        high = np.array([1., 1., self.max_speed])
-        self.action_space = spaces.Box(low=-self.max_torque, high=self.max_torque, shape=(1,), dtype=np.float32)
-        self.observation_space = spaces.Box(low=-high, high=high, dtype=np.float32)
+        high_ang = np.array([1., 1.])
+        high_ang_vel = np.array([self.max_speed])
+        self.action_space = spaces.Box(
+            low=-self.max_torque, high=self.max_torque, shape=(1,), dtype=np.float32)
+        self.observation_space = spaces.Dict(
+            {
+                'angle': spaces.Box(low=-high_ang, high=high_ang, dtype=np.float32),
+                'angular_velocity': spaces.Box(low=-high_ang_vel, high=high_ang_vel, dtype=np.float32)
+            }
+        )
 
         self.seed()
 
@@ -29,8 +37,8 @@ class PendulumDictEnv(gym.Env):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
 
-    def step(self,u):
-        th, thdot = self.state # th := theta
+    def step(self, u):
+        th, thdot = self.state  # th := theta
 
         g = 10.
         m = 1.
@@ -38,12 +46,14 @@ class PendulumDictEnv(gym.Env):
         dt = self.dt
 
         u = np.clip(u, -self.max_torque, self.max_torque)[0]
-        self.last_u = u # for rendering
+        self.last_u = u  # for rendering
         costs = angle_normalize(th)**2 + .1*thdot**2 + .001*(u**2)
 
-        newthdot = thdot + (-3*g/(2*l) * np.sin(th + np.pi) + 3./(m*l**2)*u) * dt
+        newthdot = thdot + \
+            (-3*g/(2*l) * np.sin(th + np.pi) + 3./(m*l**2)*u) * dt
         newth = th + newthdot*dt
-        newthdot = np.clip(newthdot, -self.max_speed, self.max_speed) #pylint: disable=E1111
+        newthdot = np.clip(newthdot, -self.max_speed,
+                           self.max_speed)  # pylint: disable=E1111
 
         self.state = np.array([newth, newthdot])
         return self._get_obs(), -costs, False, {}
@@ -57,23 +67,23 @@ class PendulumDictEnv(gym.Env):
     def _get_obs(self):
         theta, thetadot = self.state
         return {
-            'ang': np.array([np.cos(theta), np.sin(theta)]),
-            'ang_vel': thetadot
+            'angle': np.array([np.cos(theta), np.sin(theta)]),
+            'angular_velocity': np.array([thetadot])
         }
 
     def render(self, mode='human'):
 
         if self.viewer is None:
             from gym.envs.classic_control import rendering
-            self.viewer = rendering.Viewer(500,500)
-            self.viewer.set_bounds(-2.2,2.2,-2.2,2.2)
+            self.viewer = rendering.Viewer(500, 500)
+            self.viewer.set_bounds(-2.2, 2.2, -2.2, 2.2)
             rod = rendering.make_capsule(1, .2)
             rod.set_color(.8, .3, .3)
             self.pole_transform = rendering.Transform()
             rod.add_attr(self.pole_transform)
             self.viewer.add_geom(rod)
             axle = rendering.make_circle(.05)
-            axle.set_color(0,0,0)
+            axle.set_color(0, 0, 0)
             self.viewer.add_geom(axle)
             fname = path.join(path.dirname(__file__), "assets/clockwise.png")
             self.img = rendering.Image(fname, 1., 1.)
@@ -85,12 +95,13 @@ class PendulumDictEnv(gym.Env):
         if self.last_u:
             self.imgtrans.scale = (-self.last_u/2, np.abs(self.last_u)/2)
 
-        return self.viewer.render(return_rgb_array = mode=='rgb_array')
+        return self.viewer.render(return_rgb_array=mode == 'rgb_array')
 
     def close(self):
         if self.viewer:
             self.viewer.close()
             self.viewer = None
+
 
 def angle_normalize(x):
     return (((x+np.pi) % (2*np.pi)) - np.pi)
