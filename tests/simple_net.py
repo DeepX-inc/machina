3fcc52d1d222dc2b5983027674486b2e9c9d75dc
takerfume <yoshida.taketo1995@gmail.com>
2019-03-29 22:23:52 +0900

    [tests/simple] Add PolDictNet

--- tests/simple_net.py
+++ tests/simple_net.py
@@ -7,6 +7,7 @@ import torch.nn as nn
 from torch.nn.init import kaiming_uniform_, uniform_
 import torch.nn.functional as F
 import gym
+from machina.envs import flatten_to_dict
 
 
 def mini_weight_init(m):
@@ -325,3 +326,57 @@ class DiaynDiscrimNet(nn.Module):
         feat = self.discrim_f(ob)
         h = torch.relu(self.fc1(feat))
         return self.output_layer(h)
+
+class PolDictNet(nn.Module):
+    def __init__(self, ob_space, ac_space, dict_keys, h1=200, h2=100, deterministic=False):
+        super(PolDictNet, self).__init__()
+
+        self.ob_space = ob_space
+        self.dict_keys = dict_keys
+        self.deterministic = deterministic
+
+        if isinstance(ac_space, gym.spaces.Box):
+            self.discrete = False
+        else:
+            self.discrete = True
+            if isinstance(ac_space, gym.spaces.MultiDiscrete):
+                self.multi = True
+            else:
+                self.multi = False
+
+        self.fc1 = nn.Linear(ob_space.spaces['angle'].shape[0], h1)
+        self.fc2 = nn.Linear(h1 + self.ob_space.spaces['angular_velocity'].shape[0], h2)
+        self.fc1.apply(weight_init)
+        self.fc2.apply(weight_init)
+
+        if not self.discrete:
+            self.mean_layer = nn.Linear(h2, ac_space.shape[0])
+            if not self.deterministic:
+                self.log_std_param = nn.Parameter(
+                    torch.randn(ac_space.shape[0])*1e-10 - 1)
+            self.mean_layer.apply(mini_weight_init)
+        else:
+            if self.multi:
+                self.output_layers = nn.ModuleList(
+                    [nn.Linear(h2, vec) for vec in ac_space.nvec])
+                list(map(lambda x: x.apply(mini_weight_init), self.output_layers))
+            else:
+                self.output_layer = nn.Linear(h2, ac_space.n)
+                self.output_layer.apply(mini_weight_init)
+
+    def forward(self, ob):
+        dict_ob = flatten_to_dict(ob, self.ob_space, self.dict_keys)
+        h = F.relu(self.fc1(dict_ob['angle']))
+        h = F.relu(self.fc2(torch.cat([h, dict_ob['angular_velocity']], dim=1)))
+        if not self.discrete:
+            mean = torch.tanh(self.mean_layer(h))
+            if not self.deterministic:
+                log_std = self.log_std_param.expand_as(mean)
+                return mean, log_std
+            else:
+                return mean
+        else:
+            if self.multi:
+                return torch.cat([torch.softmax(ol(h), dim=-1).unsqueeze(-2) for ol in self.output_layers], dim=-2)
+            else:
+                return torch.softmax(self.output_layer(h), dim=-1)
