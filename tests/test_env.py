4ba5603e97a433518e9914ad06832adc2edb02d5
takerfume <yoshida.taketo1995@gmail.com>
2019-03-29 22:22:57 +0900

    [tests/test_env] Add test of flatten2dict

--- tests/test_env.py
+++ tests/test_env.py
@@ -8,8 +8,16 @@ import gym
 import numpy as np
 import torch
 
-from machina.envs import GymEnv, C2DEnv
-
+from gym.wrappers import FlattenDictWrapper
+from machina.envs import GymEnv, C2DEnv, flatten_to_dict
+from env import PendulumDictEnv
+from simple_net import PolDictNet, VNet
+from machina.vfuncs import DeterministicSVfunc
+from machina.pols import GaussianPol
+from machina.traj import Traj
+from machina.traj import epi_functional as ef
+from machina.samplers import EpiSampler
+from machina.algos import ppo_clip
 
 def test_continuous2discrete():
     continuous_env = GymEnv('Pendulum-v0', record_video=False)
@@ -19,6 +27,52 @@ def test_continuous2discrete():
 
     discrete_env.reset()
     out = discrete_env.step([3, 10])
+
+def test_flatten2dict():
+    dict_env = PendulumDictEnv()
+    dict_ob = dict_env.observation_space.sample()
+    env = FlattenDictWrapper(dict_env, dict_env.observation_space.spaces.keys())
+    flatten_ob = env.observation(dict_ob)
+    recovered_dict_ob = flatten_to_dict(flatten_ob, dict_env.observation_space, env.dict_keys)
+    tf = []
+    for (a_key, a_val), (b_key, b_val) in zip(dict_ob.items(), recovered_dict_ob.items()):
+        tf.append(a_key==b_key)
+        tf.append(all(a_val==b_val))
+    assert all(tf)
+
+class TestFlatten2Dict(unittest.TestCase):
+    def setUp(self):
+        dict_env = PendulumDictEnv()
+        self.env = FlattenDictWrapper(dict_env, dict_env.observation_space.spaces.keys())
+
+    def test_learning(self):
+        pol_net = PolDictNet(self.env.env.observation_space, self.env.action_space, self.env.dict_keys, h1=32, h2=32)
+        pol = GaussianPol(self.env.env.observation_space, self.env.action_space, pol_net)
+
+        vf_net = VNet(self.env.observation_space, h1=32, h2=32)
+        vf = DeterministicSVfunc(self.env.env.observation_space, vf_net)
+
+        sampler = EpiSampler(self.env, pol, num_parallel=1)
+
+        optim_pol = torch.optim.Adam(pol_net.parameters(), 3e-4)
+        optim_vf = torch.optim.Adam(vf_net.parameters(), 3e-4)
+
+        epis = sampler.sample(pol, max_steps=32)
+
+        traj = Traj()
+        traj.add_epis(epis)
+
+        traj = ef.compute_vs(traj, vf)
+        traj = ef.compute_rets(traj, 0.99)
+        traj = ef.compute_advs(traj, 0.99, 0.95)
+        traj = ef.centerize_advs(traj)
+        traj = ef.compute_h_masks(traj)
+        traj.register_epis()
+
+        result_dict = ppo_clip.train(traj=traj, pol=pol, vf=vf, clip_param=0.2,
+                                     optim_pol=optim_pol, optim_vf=optim_vf, epoch=1, batch_size=32)
+
+        del sampler
 
 
 if __name__ == '__main__':
