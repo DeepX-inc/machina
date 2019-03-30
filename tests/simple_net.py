bdeeafff256bb0e4e55c3f5dbdc7c85fb926b176
takerfume <yoshida.taketo1995@gmail.com>
2019-03-31 00:14:42 +0900

    [tests/simple_net] Support rnn

--- tests/simple_net.py
+++ tests/simple_net.py
@@ -382,3 +382,74 @@ class PolDictNet(nn.Module):
                 return torch.cat([torch.softmax(ol(h), dim=-1).unsqueeze(-2) for ol in self.output_layers], dim=-2)
             else:
                 return torch.softmax(self.output_layer(h), dim=-1)
+
+
+class PolNetDictLSTM(nn.Module):
+    def __init__(self, ob_space, ac_space, h_size=1024, cell_size=512):
+        super(PolNetDictLSTM, self).__init__()
+        self.h_size = h_size
+        self.cell_size = cell_size
+        self.rnn = True
+        self.ob_space = ob_space
+
+        if isinstance(ac_space, gym.spaces.Box):
+            self.discrete = False
+        else:
+            self.discrete = True
+            if isinstance(ac_space, gym.spaces.MultiDiscrete):
+                self.multi = True
+            else:
+                self.multi = False
+
+        self.input_layer = nn.Linear(
+            ob_space.spaces['angle'].shape[0], self.h_size)
+        self.cell = nn.LSTMCell(
+            self.h_size + ob_space.spaces['angular_velocity'].shape[0], hidden_size=self.cell_size)
+        if not self.discrete:
+            self.mean_layer = nn.Linear(self.cell_size, ac_space.shape[0])
+            self.log_std_param = nn.Parameter(
+                torch.randn(ac_space.shape[0])*1e-10 - 1)
+
+            self.mean_layer.apply(mini_weight_init)
+        else:
+            if self.multi:
+                self.output_layers = nn.ModuleList(
+                    [nn.Linear(self.cell_size, vec) for vec in ac_space.nvec])
+                list(map(lambda x: x.apply(mini_weight_init), self.output_layers))
+            else:
+                self.output_layer = nn.Linear(self.cell_size, ac_space.n)
+                self.output_layer.apply(mini_weight_init)
+
+    def init_hs(self, batch_size=1):
+        new_hs = (next(self.parameters()).new(batch_size, self.cell_size).zero_(), next(
+            self.parameters()).new(batch_size, self.cell_size).zero_())
+        return new_hs
+
+    def forward(self, xs, hs, h_masks):
+        print(xs.shape)
+        time_seq, batch_size, *_ = xs.shape
+
+        hs = (hs[0].reshape(batch_size, self.cell_size),
+              hs[1].reshape(batch_size, self.cell_size))
+
+        dict_xs = flatten_to_dict(xs, self.ob_space)
+        xs = torch.relu(self.input_layer(dict_xs['angle']))
+        ang_vels = dict_xs['angular_velocity']
+
+        hiddens = []
+        for x, ang_vel, mask in zip(xs, ang_vels, h_masks):
+            hs = (hs[0] * (1 - mask), hs[1] * (1 - mask))
+            hs = self.cell(
+                torch.cat([x, ang_vel], dim=1), hs)
+            hiddens.append(hs[0])
+        hiddens = torch.cat([h.unsqueeze(0) for h in hiddens], dim=0)
+
+        if not self.discrete:
+            means = torch.tanh(self.mean_layer(hiddens))
+            log_std = self.log_std_param.expand_as(means)
+            return means, log_std, hs
+        else:
+            if self.multi:
+                return torch.cat([torch.softmax(ol(hiddens), dim=-1).unsqueeze(-2) for ol in self.output_layers], dim=-2), hs
+            else:
+                return torch.softmax(self.output_layer(hiddens), dim=-1), hs
