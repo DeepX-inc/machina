517b749b0f8c5bf5d251e47d75f027e245018eb7
takerfume <yoshida.taketo1995@gmail.com>
2019-04-01 09:27:01 +0900

    [tests/test_env] Add r2d2 test

--- tests/test_env.py
+++ tests/test_env.py
@@ -11,13 +11,13 @@ from torch import nn
 
 from gym.wrappers import FlattenDictWrapper
 from machina.envs import GymEnv, C2DEnv, flatten_to_dict
-from simple_net import PolDictNet, VNet, QNet, VNetLSTM, PolNetDictLSTM
+from simple_net import PolDictNet, VNet, QNet, VNetLSTM, PolNetDictLSTM, QNetLSTM
 from machina.vfuncs import DeterministicSVfunc, DeterministicSAVfunc
 from machina.pols import GaussianPol
 from machina.traj import Traj
 from machina.traj import epi_functional as ef
 from machina.samplers import EpiSampler
-from machina.algos import ppo_clip, sac
+from machina.algos import ppo_clip, sac, r2d2_sac
 from gym.envs import register
 
 register(
@@ -181,6 +181,81 @@ class TestFlatten2DictSAC(unittest.TestCase):
             pol, qfs, targ_qfs, log_alpha,
             optim_pol, optim_qfs, optim_alpha,
             2, 32,
+            0.01, 0.99, 2,
+        )
+
+        del sampler
+
+
+class TestFlatten2DictR2D2SAC(unittest.TestCase):
+    def setUp(self):
+        dict_env = gym.make('PendulumDictEnv-v0')
+        self.dict_ob_space = dict_env.observation_space
+        env = FlattenDictWrapper(
+            dict_env, dict_env.observation_space.spaces.keys())
+        self.env = GymEnv(env)
+
+    def test_learning(self):
+        pol_net = PolNetDictLSTM(
+            self.dict_ob_space, self.env.ac_space, h_size=32, cell_size=32)
+        pol = GaussianPol(self.env.ob_space,
+                          self.env.ac_space, pol_net, rnn=True)
+
+        qf_net1 = QNetLSTM(self.env.ob_space,
+                           self.env.ac_space, h_size=32, cell_size=32)
+        qf1 = DeterministicSAVfunc(
+            self.env.ob_space, self.env.ac_space, qf_net1, rnn=True)
+        targ_qf_net1 = QNetLSTM(
+            self.env.ob_space, self.env.ac_space, h_size=32, cell_size=32)
+        targ_qf_net1.load_state_dict(qf_net1.state_dict())
+        targ_qf1 = DeterministicSAVfunc(
+            self.env.ob_space, self.env.ac_space, targ_qf_net1, rnn=True)
+
+        qf_net2 = QNetLSTM(self.env.ob_space,
+                           self.env.ac_space, h_size=32, cell_size=32)
+        qf2 = DeterministicSAVfunc(
+            self.env.ob_space, self.env.ac_space, qf_net2, rnn=True)
+        targ_qf_net2 = QNetLSTM(
+            self.env.ob_space, self.env.ac_space, h_size=32, cell_size=32)
+        targ_qf_net2.load_state_dict(qf_net2.state_dict())
+        targ_qf2 = DeterministicSAVfunc(
+            self.env.ob_space, self.env.ac_space, targ_qf_net2, rnn=True)
+
+        qfs = [qf1, qf2]
+        targ_qfs = [targ_qf1, targ_qf2]
+
+        log_alpha = nn.Parameter(torch.zeros(()))
+
+        sampler = EpiSampler(self.env, pol, num_parallel=1)
+
+        optim_pol = torch.optim.Adam(pol_net.parameters(), 3e-4)
+        optim_qf1 = torch.optim.Adam(qf_net1.parameters(), 3e-4)
+        optim_qf2 = torch.optim.Adam(qf_net2.parameters(), 3e-4)
+        optim_qfs = [optim_qf1, optim_qf2]
+        optim_alpha = torch.optim.Adam([log_alpha], 3e-4)
+
+        epis = sampler.sample(pol, max_steps=32)
+
+        traj = Traj()
+        traj.add_epis(epis)
+
+        traj = ef.add_next_obs(traj)
+        max_pri = traj.get_max_pri()
+        traj = ef.set_all_pris(traj, max_pri)
+        traj = ef.compute_seq_pris(traj, 4)
+        traj = ef.compute_h_masks(traj)
+        for i in range(len(qfs)):
+            traj = ef.compute_hs(
+                traj, qfs[i], hs_name='q_hs'+str(i), input_acs=True)
+            traj = ef.compute_hs(
+                traj, targ_qfs[i], hs_name='targ_q_hs'+str(i), input_acs=True)
+        traj.register_epis()
+
+        result_dict = r2d2_sac.train(
+            traj,
+            pol, qfs, targ_qfs, log_alpha,
+            optim_pol, optim_qfs, optim_alpha,
+            2, 32, 4, 2,
             0.01, 0.99, 2,
         )
 
